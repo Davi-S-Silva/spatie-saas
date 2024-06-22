c7e9843904bf063b92dd40abda1b8173ee39264d
davi-s-silva <daviedeus43@gmail.com>
2024-06-22 11:57:27 -0300

    correcao bug git

--- resources/views/entrega/create.blade.php
+++ resources/views/entrega/create.blade.php
@@ -1,22 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-<<<<<<< HEAD
-            {{ __('Entregas') }}
-=======
             {{ __('Nova Entrega') }}
->>>>>>> 506fcbab70c9ebb8befefd76a798fab567186cf2
         </h2>
     </x-slot>
     <div class="py-1">
         <div class="max-w-7xl mx-auto px-1">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="card col-12 p-2">
-<<<<<<< HEAD
-                    <form action="{{ route('entrega.store') }}" name="FormEntrega" method="post">
-                        @include('entrega.form-entrega')
-                    </form>
-=======
                     @foreach ($clientes as $cliente)
                         <ul>
                             {{-- <li><b><a href="{{ route('clientes.show',['cliente'=>$cliente->id]) }}">{{ $cliente->name }}</b></a> --}}
@@ -28,7 +19,6 @@
                             </li>
                         </ul>
                     @endforeach
->>>>>>> 506fcbab70c9ebb8befefd76a798fab567186cf2
                 </div>
             </div>
         </div>
