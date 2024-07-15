7e3095f7247fdcb06929c66b5d5b74d1c23a22ef
davi-s-silva <69742480+Davi-S-Silva@users.noreply.github.com>
2024-07-15 10:26:39 -0300

    reordenar seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,14 +21,16 @@ class UserSeeder extends Seeder
             'password' => Hash::make('password'),
             // 'empresa_id'=>1,
         ]);
-        // DB::table('users')->insert([
-        //     'name' => 'Marcilene Celestino da Silva',
-        //     'email' => 'marcilenecelestino907@gmail.com',
-        //     'password' => Hash::make('password'),
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'Marcilene Celestino da Silva',
+            'email' => 'marcilenecelestino907@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
         $user = User::where('email','adminmaster@gmail.com')->get()->first();
         $user->syncRoles(['super-admin']);
-        // $user = User::where('email','marcilenecelestino907@gmail.com')->get()->first();
-        // $user->syncRoles(['admin']);
+
+        $user = User::where('email','marcilenecelestino907@gmail.com')->get()->first();
+        $user->syncRoles(['admin']);
     }
 }
